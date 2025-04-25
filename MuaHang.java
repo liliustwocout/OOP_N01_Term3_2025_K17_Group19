b2b0d8c0de4e6a9bac279ca01e3b807c060718ac
Thành Đạt <98679017+liliusgamer@users.noreply.github.com>
2025-04-25 14:11:38 +0700

    Add files via upload

---
+++ MuaHang.java
@@ -0,0 +1,7 @@
+import SanPham;
+import KhachHang;
+
+public class MuaHang {
+    //Liên quan thông tin đối tượng sản phẩm
+    //Liên quan thong tin đối tượng khách hàng
+}
---
+++ SanPham.java
@@ -0,0 +1,11 @@
+public class SanPham {
+    //Biến miêu tả
+    String TenSP;
+    String MaSP;
+    //Date NgaySX;
+
+    //Phương thức hoat động đối tượng sản phẩm
+    public String HienThiSanPham(String MaSP){
+        return TenSP;
+    }
+}
