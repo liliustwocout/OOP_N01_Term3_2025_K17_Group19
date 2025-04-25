6cf6fc845696f1f5cc995f91a14373e132d145f8
Thành Đạt <98679017+liliusgamer@users.noreply.github.com>
2025-04-25 07:59:16 +0000

    alo

---
+++ testt/src/KhachHang.java
@@ -0,0 +1,11 @@
+public class KhachHang {
+    //Biến miêu tả
+    String TenKH;
+    String MaKH;
+    //Date NgaySinh;
+
+    //Phương thức hoat động đối tượng sản phẩm
+    public String HienThiKhachHang(String MaSKH){
+        return TenKH;
+    }
+}
