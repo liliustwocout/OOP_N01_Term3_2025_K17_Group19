c94344f54891de94c58683f1decd2d2d44988899
Thành Đạt <98679017+liliusgamer@users.noreply.github.com>
2025-04-27 16:16:39 +0700

    Add files via upload

--- KhachHang.java
+++ KhachHang.java
@@ -3,13 +3,20 @@ public class KhachHang {
     String TenKH;
     String MaKH;
     //Date NgaySinh;
-    //abcxyz
-
-
+    String Sodienthoai;
+    String Diachi;
+    String Email;
 
 
     //Phương thức hoat động đối tượng sản phẩm
-    public String HienThiKhachHang(String MaSKH){
+    public String HienThiKhachHang(String MaKH){
         return TenKH;
+    }
+    public KhachHang(String TenKH, String MaKH, String Sodienthoai, String Diachi, String Email){
+        this.TenKH = TenKH;
+        this.MaKH = MaKH;
+        this.Sodienthoai = Sodienthoai;
+        this.Diachi = Diachi;
+        this.Email = Email;
     }
 }
