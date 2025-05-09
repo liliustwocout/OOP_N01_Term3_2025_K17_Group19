fee2b04fbacea40a0d2d4f3856945dae5c743095
Thành Đạt <98679017+liliusgamer@users.noreply.github.com>
2025-05-09 16:37:42 +0000

    UPDATED PlayerTest

---
+++ src/characters/Player.java
@@ -0,0 +1,27 @@
+// src/characters/Player.java
+package characters;
+
+public class Player {
+    private String name;
+    public int health;
+    private int x, y;
+
+    public Player(String name, int health, int x, int y) {
+        this.name = name;
+        this.health = health;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move(String direction) {
+        System.out.println(name + " is moving " + direction);
+    }
+
+    public void attack() {
+        System.out.println(name + " attacks!");
+    }
+
+    public void takeDamage(int amount) {
+        health -= amount;
+    }
+}
---
+++ test/PlayerTest.java
@@ -0,0 +1,15 @@
+// src/tests/PlayerTest.java
+package test;
+
+import characters.Player;
+
+
+public class PlayerTest {
+
+    
+    public void testTakeDamage() {
+        Player p = new Player("Hero", 100, 0, 0);
+        p.takeDamage(30);
+
+    }
+}
