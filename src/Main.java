d40e97aed2a87aea905e4d5bedb2ee6004784ebd
Đạt <datpltn205@gmail.com>
2025-06-21 16:22:52 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
-import test.testCombat;
-import test.testPlayer;
+// import test.testCombat;
+// import test.testPlayer;
 
-public class Main {
-    public static void main(String[] args) {
-        // testCombat tc = new testCombat();
-        // tc.testCombat(); 
-        testPlayer tp = new testPlayer();
-        tp.testShowHighLevelPlayers();
-    }
-}
+// public class Main {
+//     public static void main(String[] args) {
+//         // testCombat tc = new testCombat();
+//         // tc.testCombat(); 
+//         testPlayer tp = new testPlayer();
+//         tp.testShowHighLevelPlayers();
+//     }
+// }
 
 
